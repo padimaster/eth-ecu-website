24a457682a47243f7634c89400bb452ba663a0b5
Alex Padilla <alex.padilla@epn.edu.ec>
2025-01-23 00:29:23 -0500

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,17 @@ export const metadata: Metadata = {
     images: ["/opengraph-image.png"],
     creator: "@EthTricolor",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
   alternates: {
     canonical: "https://ethecuador.org",
   },
