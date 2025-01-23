d4e6e6545148aa74748243f7e1fc9c39a8b631bb
Alex Padilla <alex.padilla@epn.edu.ec>
2025-01-23 00:41:19 -0500

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -74,6 +75,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
